04a2a9d83df538a8404c8358ecc0fe812f4f7604
filtoid <philjeffes@hotmail.com>
2023-07-03 18:56:08 +0200

    example code,
    rotating green clockwise

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,10 @@ use std::fs;
 
 const WIDTH: u32 = 600;
 const HEIGHT: u32 = 600;
-const FRAMES: u32 = 120;
+const FRAMES: u32 = 60;
 const REPETITIONS: u32 = 3;
-// const SIZE: u32 = 128;
-const DECAY: u32 = 4;
+const SIZE: u32 = 16;
+const DECAY: u32 = 10;
 
 fn main() {
     println!("Starting");
@@ -80,8 +80,24 @@ fn main() {
         // Perform caluclation of current frame state
         let frame_fraction = frame as f32 / FRAMES as f32;
         print!("\rRendering Frames: {:.2}%", frame_fraction * 50.0);
-        //// #### TODO: Put your code here
         
+        let fraction_radian = frame_fraction * (std::f32::consts::PI * 2.0);
+        let half_width: f32 = WIDTH as f32/2.0;
+        let offset_x =  (WIDTH as i32/2) + (fraction_radian.sin() * (half_width * 0.8)) as i32;
+        let half_height: f32 = HEIGHT as f32/2.0 ;
+        let offset_y: i32 = (HEIGHT as i32/2) - (fraction_radian.cos() * (half_height * 0.8)) as i32;
+        // set a central pixel to white
+        for x in 0..SIZE {
+            for y in 0..SIZE {
+                let x_pos = x + offset_x as u32;
+                let y_pos = y + offset_y as u32;
+
+                if x_pos > 0 && x_pos < WIDTH && y_pos > 0 && y_pos < HEIGHT {
+                    image.put_pixel(x_pos, y_pos, Rgb([55, 255, 55]));
+                }
+            
+            }
+        }
         // End frame generation
 
         // Generate next frame for output - we run the animation twice and capture the second half of the first reptition
