4330bccaaabf6c8bc4b9ec543f2f833e14d380d5
Filtoid <philjeffes@hotmail.com>
2023-08-15 22:33:25 +0200

    moved code to separate file

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,58 @@ use image::{RgbImage,Rgb};
 
 use minimp4;
 use openh264;
-use rand::prelude::*;
 
 use std::io::{Cursor, Read, Seek, SeekFrom};
 use std::fs;
 
-const WIDTH: u32 = 600;
-const HEIGHT: u32 = 600;
-const FRAMES: u32 = 240;
+mod user;
+
+const WIDTH: u32 = 300;
+const HEIGHT: u32 = 300;
+const FRAMES: u32 = 60;
 const REPETITIONS: u32 = 3;
-const NUM_VORTICES: u32 = 40;
+const NUM_VORTICES: u32 = 5;
 const DECAY: u32 = 4;
 
-struct Location<T> {
-    x: T,
-    y: T
-}
-
-struct VortexPoint {
-    center: Location<u32>,
-    color: Rgb<u8>,
-    rotation: f32,
-    size: u32,
-    width: u32
+fn prepare_next_frame(prev_image: Option<RgbImage>) -> RgbImage {
+    let mut image: RgbImage = RgbImage::new(WIDTH, HEIGHT);
+    // Copy previous image or create new
+    for x in 0..WIDTH {
+        for y in 0..HEIGHT {
+            match prev_image {
+                Some(ref prev_image) => {
+                    let mut pix_col: Rgb<u8> = *prev_image.get_pixel(x,y);
+                    // Add decay to any non-black pixel
+                    if pix_col[0] != 0 {
+                        if pix_col[0] >= DECAY as u8 {
+                            pix_col[0] -= DECAY as u8;
+                        }else{
+                            pix_col[0] = 0;
+                        }
+                    }
+                    if pix_col[1] != 0 {
+                        if pix_col[1] >= DECAY as u8 {
+                            pix_col[1] -= DECAY as u8;
+                        }else{
+                            pix_col[1] = 0;
+                        }
+                    }
+                    if pix_col[2] != 0 {
+                        if pix_col[2] >= DECAY as u8 {
+                            pix_col[2] -= DECAY as u8;
+                        }else{
+                            pix_col[2] = 0;
+                        }
+                    }
+                    image.put_pixel(x,y,pix_col);
+                },
+                None => {
+                    image.put_pixel(x, y, Rgb([0, 0, 0]));
+                }
+            }
+        }
+    }
+    image
 }
 
 fn main() {
@@ -49,95 +78,18 @@ fn main() {
     let mut prev_image: Option<RgbImage> = None;
     let mut buf = Vec::new();
 
-    let mut vortices: Vec::<VortexPoint> = Vec::new();
-    let center = Location{x: WIDTH/2, y:HEIGHT/2};
-    let mut rng = rand::thread_rng();
-    for _ in 0..NUM_VORTICES {
-        let diff_x: i32 = rng.gen_range(-10..10);
-        let diff_y: i32 = rng.gen_range(-10..10);
-        let red: u8 = rng.gen_range(55..255);
-        let green: u8 = rng.gen_range(55..255);
-        let blue: u8 = rng.gen_range(55..255);
-        let rot: f32 = rng.gen_range(0.0..(2.0 * std::f32::consts::PI));
-        let circle_size: u32 =  rng.gen_range(40..200);
+    let mut state = user::get_initial_state();
 
-        let nxt = VortexPoint{
-            center: Location{x: (center.x as i32 + diff_x) as u32, y: (center.y as i32 + diff_y) as u32},
-            color: Rgb([red,green,blue]),
-            rotation: rot,
-            size: 5,
-            width: circle_size
-        };
-        vortices.push(nxt);
-    }
-
-    for frame in 0..FRAMES*2 {
-        let mut image: RgbImage = RgbImage::new(WIDTH, HEIGHT);
-
-        // Copy previous image or create new
-        for x in 0..WIDTH {
-            for y in 0..HEIGHT {
-                match prev_image {
-                    Some(ref prev_image) => {
-                        let mut pix_col: Rgb<u8> = *prev_image.get_pixel(x,y);
-                        // Add decay to any non-black pixel
-                        if pix_col[0] != 0 {
-                            if pix_col[0] >= DECAY as u8 {
-                                pix_col[0] -= DECAY as u8;
-                            }else{
-                                pix_col[0] = 0;
-                            }
-                        }
-                        if pix_col[1] != 0 {
-                            if pix_col[1] >= DECAY as u8 {
-                                pix_col[1] -= DECAY as u8;
-                            }else{
-                                pix_col[1] = 0;
-                            }
-                        }
-                        if pix_col[2] != 0 {
-                            if pix_col[2] >= DECAY as u8 {
-                                pix_col[2] -= DECAY as u8;
-                            }else{
-                                pix_col[2] = 0;
-                            }
-                        }
-                        image.put_pixel(x,y,pix_col);
-                    },
-                    None => {
-                        image.put_pixel(x, y, Rgb([0, 0, 0]));
-                    }
-                }
-            }
-        }
+    for frame in 0..(3*FRAMES/2) {
+        let mut image: RgbImage = prepare_next_frame(prev_image);
 
         // Perform caluclation of current frame state
         let frame_fraction = frame as f32 / FRAMES as f32;
         print!("\rRendering Frames: {:.2}%", frame_fraction * 50.0); // Multiplied by 50 because we render rounds of the 
                                                                      // animation loop, to catch any decaying pixels
-        //// #### TODO: Put your code here
-        let frame_fraction_radian = frame_fraction * 2.0 * std::f32::consts::PI;
-
-        for vp in vortices.iter_mut() {
-            let half_size = vp.size/2;
-            for x in 0..vp.size {
-                for y in 0..vp.size {
-                    let center_x: i32 = vp.center.x as i32 - half_size as i32 + x as i32;
-                    let rot_x = (frame_fraction_radian + vp.rotation).sin() ;
-                    let pos_x = center_x + (rot_x * (vp.width as f32)) as i32;
-                    
-                    let center_y: i32 = vp.center.x as i32 - half_size as i32 + y as i32;
-                    let rot_y = (frame_fraction_radian + vp.rotation).cos();
-                    let pos_y = center_y + (rot_y * (vp.width as f32)) as i32;
-
-                    if pos_x >= 0 && pos_x < WIDTH as i32 && pos_y >= 0 && pos_y < HEIGHT as i32 {
-                        image.put_pixel(pos_x as u32,pos_y as u32,vp.color);
-                    }
-                }
-            }
-        }
-        // End frame generation
-
+ 
+        user::render_frame(&mut image, frame_fraction, &mut state);
+ 
         // Generate next frame for output - we run the animation twice and capture the second half of the first reptition
         // and the first half of the second repetition - meaning we can get any fading colors captured as well
         if frame >= FRAMES / 2 && frame < (FRAMES*3/2) {
---
+++ src/user.rs
@@ -0,0 +1,74 @@
+use rand::prelude::*;
+use image::{RgbImage, Rgb};
+use std::vec; 
+
+use crate::{WIDTH,HEIGHT,NUM_VORTICES};
+
+struct Location<T> {
+    x: T,
+    y: T
+}
+
+struct VortexPoint {
+    center: Location<u32>,
+    color: Rgb<u8>,
+    rotation: f32,
+    size: u32,
+    width: u32
+}
+
+pub struct State {
+    vortices: vec::Vec::<VortexPoint>
+}
+
+pub fn get_initial_state() -> State {
+    
+    let mut vortices: Vec::<VortexPoint> = Vec::new();
+    let center = Location{x: WIDTH/2, y:HEIGHT/2};
+    let mut rng = rand::thread_rng();
+    for _ in 0..NUM_VORTICES {
+        let diff_x: i32 = rng.gen_range(-10..10);
+        let diff_y: i32 = rng.gen_range(-10..10);
+        let red: u8 = rng.gen_range(55..255);
+        let green: u8 = rng.gen_range(55..255);
+        let blue: u8 = rng.gen_range(55..255);
+        let rot: f32 = rng.gen_range(0.0..(2.0 * std::f32::consts::PI));
+        let circle_size: u32 =  rng.gen_range(40..200);
+
+        let nxt = VortexPoint{
+            center: Location{x: (center.x as i32 + diff_x) as u32, y: (center.y as i32 + diff_y) as u32},
+            color: Rgb([red,green,blue]),
+            rotation: rot,
+            size: 5,
+            width: circle_size
+        };
+        vortices.push(nxt);
+    }
+    let ret = State{vortices: vortices};
+    ret
+}
+
+pub fn render_frame(frame: &mut RgbImage, fraction: f32, state: &mut State) {
+   
+    let frame_fraction_radian = fraction * 2.0 * std::f32::consts::PI;
+
+    for vp in state.vortices.iter_mut() {
+        let half_size = vp.size/2;
+        for x in 0..vp.size {
+            for y in 0..vp.size {
+                let center_x: i32 = vp.center.x as i32 - half_size as i32 + x as i32;
+                let rot_x = (frame_fraction_radian + vp.rotation).sin() ;
+                let pos_x = center_x + (rot_x * (vp.width as f32)) as i32;
+                
+                let center_y: i32 = vp.center.x as i32 - half_size as i32 + y as i32;
+                let rot_y = (frame_fraction_radian + vp.rotation).cos();
+                let pos_y = center_y + (rot_y * (vp.width as f32)) as i32;
+
+                if pos_x >= 0 && pos_x < WIDTH as i32 && pos_y >= 0 && pos_y < HEIGHT as i32 {
+                    frame.put_pixel(pos_x as u32,pos_y as u32,vp.color);
+                }
+            }
+        }
+    }
+
+}
