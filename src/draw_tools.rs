e1cc098d749c61fa07ce13f3ac153d06d7df1e7b
filtoid <philjeffes@hotmail.com>
2023-08-29 15:04:17 +0200

    remove unneeded line

--- src/draw_tools.rs
+++ src/draw_tools.rs
@@ -21,7 +21,6 @@ pub fn draw_circle(frame: &mut RgbaImage, color: Rgba<u8>, location: &Location<u
     for x in (location.x - (size/2))..(location.x + (size/2)) {
         for y in (location.y - (size/2))..(location.y + (size/2)) {
             if x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT {
-                let mut put_pix = false;
                 let x_diff = (x as i32 - location.x as i32).abs();
                 let y_diff = (y as i32 - location.y as i32).abs();
                 if ( (x_diff*x_diff) as f32 + (y_diff * y_diff) as f32 ).sqrt() <= (size/2) as f32 {
